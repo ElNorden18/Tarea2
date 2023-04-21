aaafda63985449c0df79713fc4d99a1476215f44
ElNorden18 <pedronorden@gmail.com>
2023-04-21 15:55:38 -0400

    Menu y struct creados

--- main.c
+++ main.c
@@ -2,9 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Map.h"
+#include "stack.h"
+#include "list.h"
+
+typedef struct 
+{
+    int puntosHabilidad;
+    List *items;
+    Stack *funcionesAnteriores;
+}Datos;
+
+int is_equal_string(void *key1, void *key2)
+{
+    if (strcmp((char *)key1, (char *)key2) == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
 
 int main()
 {
-    printf("hola");
+    Map *jugadores = createMap(is_equal_string);
+    int opcion = 1;
+    while(opcion != 0)
+    {
+        printf("Menu de acciones:\n");
+        printf("1. Crear perfil de jugador\n");
+        printf("2. Mostrar perfil de jugador\n");
+        printf("3. Agregar item a jugador\n");
+        printf("4. Eliminar item de jugador\n");
+        printf("5. Agregar puntos de habilidad a jugador\n");
+        printf("6. Mostrar jugadores con item especifico\n");
+        printf("7. Deshacer ultima accion\n");
+        printf("8. Exportar datos de jugadores a archivo de texto\n");
+        printf("9. Importar datos de jugadores desde archivo de texto\n");
+        printf("0. Salir\n");
+        printf("Ingrese la opcion deseada: ");
+        scanf("%d", &opcion);
+        switch (opcion)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6: 
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 0:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opcion no valida\n");
+                break;
+        }
+
+    }
     return 0;
 }
